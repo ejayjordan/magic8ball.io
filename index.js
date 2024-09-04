516a271d8374d21ab8ba9c7ada2f20b00185cca9
jay <emma.jordan@bsu.edu>
2024-09-04 17:12:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var Eball = document.querySelector('#Eball');
+var index = 0;
+
+const fortune = [];
+    fortune[1] = "A beautiful, smart, and loving person will be coming into your life.";
+    fortune[2] = "Any day above ground is a good day.";
+    fortune[3] = "Be careful or you could fall for some tricks today.";
+    fortune[4] = "Eat dirt";
+    fortune[5] = "Courtesy begins in the home.";
+    fortune[6] = "Signs point to yes";
+    
+
+function shakeBall() {
+    var newElement = document.createElement();
+    newElement.textContent = listContent
+    document.getElementById("fortune")
+    Eball.appendChild(newE)
+}
